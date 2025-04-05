34233270ef36db2fb151cd31d2a7603653ec4aaa
adamforbes92 <34219819+adamforbes92@users.noreply.github.com>
2025-04-05 12:05:00 +0100

    V1.02
    Revised code to include multiple resolutions.

--- Code/SpeedPulserESP32.ino
+++ Code/SpeedPulserESP32.ino
@@ -1,7 +1,7 @@
 /*
 SpeedPulser - Forbes Automotive '25
 VW MK1 & MK2 analog speed converter.  Takes a 5v/12v square wave input from Can2Cluster or an OEM Hall Sensor and converts it into a PWM signal for a motor.  To get speeds low enough, the motor voltage needs reduced
-from 12v to 9v.  This allows <10mph readings while still allowing high (160mph) readings.  Clusters supported are 1540 (rotations per mile) =~ (1540*160)/60 = 4100rpm
+from 12v to ~9v.  This allows <10mph readings while still allowing high (160mph) readings.  Clusters supported are 1540 (rotations per mile) =~ (1540*160)/60 = 4100rpm
 
 Also supports 12v hall sensors from 02J / 02M etc.  According to documentation, 1Hz = 1km/h
 
@@ -36,16 +36,16 @@ void setup() {
   basicInit();                                                // init PWM, Serial, Pin IO etc.  Kept in '_io.ino' for cleanliness due to the number of Serial outputs
   motorPWM->setPWM(pinMotorOutput, pwmFrequency, dutyCycle);  // set motor to off in first instance (100% duty)
 
-#if hasNeedleSweep
-  needleSweep();
-#endif
+  if (hasNeedleSweep) {
+    needleSweep();  // enable needle sweep (in _io.ino)
+  }
 }
 
 void loop() {
   if (ledCounter > averageFilter) {
-    ledOnboard = !ledOnboard;
-    ledCounter = 0;
-    digitalWrite(pinOnboardLED, ledOnboard);
+    ledOnboard = !ledOnboard;                 // flip-flop the led trigger
+    digitalWrite(pinOnboardLED, ledOnboard);  // flash the LED to show the presence of incoming pulses
+    ledCounter = 0;                           // reset the counter
   }
 
   if (testSpeedo) {
@@ -54,8 +54,10 @@ void loop() {
     if (dutyCycle != dutyCycleIncoming) {  // only update PWM IF speed has changed (can cause flicker otherwise)
       if (incomingType == 0) {             // if 'Can2Cluster'
         DEBUG_PRINTF("     DutyIncoming: %d", dutyCycleIncoming);
+        dutyCycleIncoming = map(dutyCycleIncoming, minFreqCAN, maxFreqCAN, minSpeed, maxSpeed);  // map incoming range to this codes range.  Max Hz should match Max Speed - i.e., 200Hz = 200kmh, or 500Hz = 200kmh...
+        DEBUG_PRINTF("     DutyPostProc1: %d", dutyCycle);
 
-        // build up the array to get the average
+        // build up the array to get the average of pulsers and ignore outliars
         if (rawCount < averageFilter - 1) {
           samples.add(dutyCycleIncoming);
           rawCount++;
@@ -63,8 +65,7 @@ void loop() {
           dutyCycle = samples.getAverage(averageFilter / 2);
           DEBUG_PRINTF("     getAverage: %d", dutyCycle);
 
-          dutyCycle = findClosestMatch(dutyCycle);  // find the closest final duty based on the incoming duty (use motor perfomance)
-
+          dutyCycle = findClosestMatch(dutyCycle);             // find the closest final duty based on the incoming duty (use motor perfomance array)
           motorPWM->setPWM_manual(pinMotorOutput, dutyCycle);  // set the duty of the motor from the calculations
           DEBUG_PRINTF("     FindClosetMatch: %d", dutyCycle);
 
@@ -75,7 +76,7 @@ void loop() {
 
       if (incomingType == 1) {  // if 'Hall'
         DEBUG_PRINTF("     DutyIncoming: %d", dutyCycleIncoming);
-        dutyCycleIncoming = map(dutyCycleIncoming, minFreqHall, maxFreqHall, minSpeed, maxSpeed);  // map incoming range to this codes range.  Should match, but sense check...
+        dutyCycleIncoming = map(dutyCycleIncoming, minFreqHall, maxFreqHall, minSpeed, maxSpeed);  // map incoming range to this codes range.  Max Hz should match Max Speed - i.e., 200Hz = 200kmh, or 500Hz = 200kmh...
         DEBUG_PRINTF("     DutyPostProc1: %d", dutyCycle);
 
         if (rawCount < averageFilter - 1) {
@@ -93,19 +94,19 @@ void loop() {
           samples.clear();
         }
       }
-      dutyCycle = dutyCycleIncoming;  //potentially re-introduce?
+      dutyCycle = dutyCycleIncoming;  // re-introduce?
       DEBUG_PRINTLN("");
     }
   }
 }
 
-uint8_t findClosestMatch(uint8_t val) {
-  uint8_t closest = 0;
-  for (uint8_t i = 0; i < sizeof motorPerformance / sizeof motorPerformance[0]; i++) {
+uint16_t findClosestMatch(uint16_t val) {
+  uint16_t closest = 0;
+  for (uint16_t i = 0; i < sizeof motorPerformance / sizeof motorPerformance[0]; i++) {
     if (abs(val - closest) >= abs(val - motorPerformance[i]))
       closest = motorPerformance[i];
   }
-  for (uint8_t i = 0; i < sizeof motorPerformance / sizeof motorPerformance[0]; i++) {
+  for (uint16_t i = 0; i < sizeof motorPerformance / sizeof motorPerformance[0]; i++) {
     if (motorPerformance[i] == closest) {
       return i;
     }
--- Code/speedPulser_io.ino
+++ Code/speedPulser_io.ino
@@ -1,13 +1,16 @@
 void basicInit() {
   DEBUG_PRINTLN("Initialising SpeedPulser...");
 
+  DEBUG_PRINTLN("Setting up LED Output...");
   pinMode(pinOnboardLED, OUTPUT);
+  digitalWrite(pinOnboardLED, ledOnboard);
+  DEBUG_PRINTLN("Set up LED Output!");
 
   DEBUG_PRINTLN("Setting up PWM...");
   motorPWM = new ESP32_FAST_PWM(pinMotorOutput, pwmFrequency, dutyCycle, 0, pwmResolution);  // begin PWM with a base freq. of 10kHz @ 100% duty (mot. off)
   DEBUG_PRINTLN("Set up PWM!");
 
-  DEBUG_PRINTLN("Setting up speed interrupt...");
+  DEBUG_PRINTLN("Setting up Speed Interrupt...");
   attachInterrupt(digitalPinToInterrupt(pinSpeedInput), incomingHz, FALLING);  //setup interrupt to toggle pin on change
   DEBUG_PRINTLN("Set up speed interrupt!");
 
@@ -15,26 +18,48 @@ void basicInit() {
 }
 
 void testSpeed() {
-  DEBUG_PRINTF("PWM: %d", tempDuty);
-  motorPWM->setPWM_manual(pinMotorOutput, tempDuty);
-  delay(sweepSpeed * 200);
+  if (tempSpeed > 0) {
+    dutyCycle = findClosestMatch(tempSpeed);  // find the closest final duty based on the incoming duty (use motor perfomance)
+    DEBUG_PRINTF("Speed: %d", tempSpeed);
+    DEBUG_PRINTLN("");
+
+    motorPWM->setPWM_manual(pinMotorOutput, dutyCycle);  // set the duty of the motor from the calculations
+    delay(sweepSpeed * 10);
+  } else {
+    for (uint8_t i = 0; i < maxSpeed; i++) { // run through all available speeds and drive the motor
+      dutyCycle = findClosestMatch(i);  // find the closest final duty based on the incoming duty (use motor perfomance)
+      DEBUG_PRINTF("Speed: %d", i);
+      DEBUG_PRINTLN("");
+
+      motorPWM->setPWM_manual(pinMotorOutput, dutyCycle);  // set the duty of the motor from the calculations
+      delay(sweepSpeed);
+    }
+  }
 }
 
 void needleSweep() {
   // ramp up
-  for (int i = 0; i < 100; i++) {
-    DEBUG_PRINTF("PWM: %d", tempDuty);
-    motorPWM->setPWM_manual(pinMotorOutput, i);
+  for (int i = 0; i < maxSpeed; i++) {
+    DEBUG_PRINTF("Speed: %d", i);
+    DEBUG_PRINTLN("");
+
+    dutyCycle = findClosestMatch(i);
+    motorPWM->setPWM_manual(pinMotorOutput, dutyCycle);
     delay(sweepSpeed);
   }
-  
+
   // pause
   delay(sweepSpeed);
 
   // ramp down
-  for (int i = 100; i > 0; i--) {
-    DEBUG_PRINTF("PWM: %d", tempDuty);
-    motorPWM->setPWM_manual(pinMotorOutput, i);
+  for (int i = maxSpeed; i > 0; i--) {  // set at >0 to stop the needle 'bouncing' when it returns to zero
+    DEBUG_PRINTF("Speed: %d", i);
+    DEBUG_PRINTLN("");
+
+    dutyCycle = findClosestMatch(i);
+    motorPWM->setPWM_manual(pinMotorOutput, dutyCycle);
     delay(sweepSpeed);
   }
+  delay(sweepSpeed);
+  motorPWM->setPWM_manual(pinMotorOutput, 0);
 }
